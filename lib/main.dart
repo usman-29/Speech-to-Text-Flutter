ecc407472529103ba0ba31b690ce7929c0119e7f
Usman Saeed <uusaeed749@gmail.com>
2023-03-11 21:33:21 +0500

    Project files

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:speechtotext/speech_screen.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SpeechScreen(),
+    );
+  }
+}
---
+++ lib/speech_screen.dart
@@ -0,0 +1,100 @@
+import 'package:avatar_glow/avatar_glow.dart';
+import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_to_text.dart';
+import 'package:speechtotext/colors.dart';
+
+class SpeechScreen extends StatefulWidget {
+  const SpeechScreen({super.key});
+
+  @override
+  State<SpeechScreen> createState() => _SpeechScreenState();
+}
+
+class _SpeechScreenState extends State<SpeechScreen> {
+  SpeechToText speechToText = SpeechToText();
+  var text = "Hold the button & start speaking";
+  var isListening = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: bgColor,
+        elevation: 0.0,
+        title: const Text(
+          "Text to Speech",
+          style: TextStyle(
+            color: textColor,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        actions: const [
+          Icon(
+            Icons.sort_rounded,
+            color: Colors.white,
+          )
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: AvatarGlow(
+        endRadius: 75.0,
+        animate: isListening,
+        duration: const Duration(milliseconds: 2000),
+        glowColor: bgColor,
+        repeat: true,
+        repeatPauseDuration: const Duration(milliseconds: 100),
+        showTwoGlows: true,
+        child: GestureDetector(
+          onTapDown: (details) async {
+            if (!isListening) {
+              var available = await speechToText.initialize();
+              if (available) {
+                setState(() {
+                  isListening = true;
+                  speechToText.listen(onResult: (result) {
+                    text = result.recognizedWords;
+                  });
+                });
+              }
+            }
+          },
+          onTapUp: (details) {
+            setState(() {
+              isListening = false;
+            });
+            speechToText.stop();
+          },
+          child: CircleAvatar(
+            backgroundColor: bgColor,
+            radius: 35,
+            child: Icon(
+              isListening ? Icons.mic : Icons.mic_none,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        reverse: true,
+        physics: const BouncingScrollPhysics(),
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height * 0.7,
+          alignment: Alignment.center,
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+          margin: const EdgeInsets.only(
+            bottom: 150,
+          ),
+          child: Text(
+            text,
+            style: TextStyle(
+              color: isListening ? Colors.black : Colors.grey,
+              fontSize: 24,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
